4923116a4750aa7e2484d61ed6eb7834d868e8c6
Trevor Royer <troyer@redhat.com>
2023-01-04 17:15:47 -0600

    update to use log level env var

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import logging
 
+from iris_message_generator.config import app_cfg
+
 from iris_message_generator import main
 
 if __name__ == "__main__":
 
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=app_cfg.log.level)
     main.load_data()
     main.message_loop()
